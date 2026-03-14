6abbabd68ad7d126ead15dc8568739c5b91ff3c5
Althaf <althafnaa@gmail.com>
2026-03-15 04:00:40 +0700

    Shift cyan to teal (more green-leaning)

--- components/TerminalLine.tsx
+++ components/TerminalLine.tsx
@@ -14,7 +14,7 @@ interface TerminalLineProps {
 export default function TerminalLine({ line }: TerminalLineProps) {
   if (line.type === "boot") {
     return (
-      <div className="text-cyan-400 whitespace-pre-wrap font-mono text-base leading-relaxed">
+      <div className="text-teal-400 whitespace-pre-wrap font-mono text-base leading-relaxed">
         {line.content}
       </div>
     );
